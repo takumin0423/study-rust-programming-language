3573814a7d719a1563a095b22396fc872c6446db
takumin0423 <software.takumi.igusa@gmail.com>
2026-02-14 21:16:36 +0900

    3. Common Programming Concepts

---
+++ chapter3/src/main.rs
@@ -0,0 +1,202 @@
+use std::io;
+
+fn main() {
+    let mut x = 5;
+
+    println!("The value of x is: {x}");
+
+    x = 6;
+
+    println!("The value of x is: {x}");
+
+    let shadowing = 5;
+    let shadowing = shadowing + 1;
+
+    {
+        let shadowing = shadowing * 2;
+
+        println!("The value of shadowing in the inner scope is: {shadowing}")
+    }
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    let sum = 5 + 10;
+    let difference = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    let remainder = 43 % 5;
+
+    println!("{truncated}");
+
+    let t = true;
+    let f: bool = false;
+
+    let c = 'z';
+    let z: char = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {y}");
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    let a = [1, 2, 3, 4, 5];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+
+    let x = five();
+
+    println!("The value of x is: {x}");
+
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    let mut count = 0;
+
+    'counting_up: loop {
+        println!("count = {count}");
+
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+
+            if remaining == 9 {
+                break;
+            }
+
+            if count == 2 {
+                break 'counting_up;
+            }
+
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+
+    println!("End count = {count}");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!");
+
+    let collection = [10, 20, 30, 40, 50];
+
+    for element in collection {
+        println!("The value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}");
+    }
+}
+
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
